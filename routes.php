639dd4d4a325b772b530151a2ce42ab4d47e66d4
nevil-ctrl <romsonsonrem@gmail.com>
2025-08-30 12:26:56 +0600

    bug

--- routes.php
+++ routes.php
@@ -4,11 +4,12 @@ require_once __DIR__.'/router.php';
 
 get('/', 'pages/index.php');
 
+get('/register', 'auth/register.php');
+any('/register','auth/register.php');
+
 get('/login', 'auth/login.php');
 any('/login','auth/login.php');
 
-get('/registr', 'auth/register.php');
-any('/registr','auth/register.php');
 
 
 any('/404','pages/404.php');
